04736caaa65e092d61f9cad97ef6f602544078e3
e9sghewariya <sghewariya@enine.dev>
2025-06-04 02:59:02 +0530

    feat(math023): Learned about Eulers project problem 23 --(part A): function to find the sum of all positive integers that cannot be expressed as the sum of two abundant numbers --(part B): made a generic function to find the sum of all positive integers that cannot be expressed as the sum of two abundant numbers upto n

---
+++ maths/20250603/math023/solution.py
@@ -0,0 +1,63 @@
+"""
+solution.py
+
+This script contains the solution to the problem of 
+finding the sum of all positive integers that cannot
+be expressed as the sum of two abundant numbers.
+"""
+def get_divisors(n):
+    """Return a list of divisors of n, excluding n itself."""
+    divisors = [1]
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n // i)
+    return divisors
+
+def check_abundant(n):
+    """Check if a number n is abundant."""
+    return sum(get_divisors(n)) > n
+
+
+def answer():
+    """Find the sum of all positive integers 
+    that cannot be expressed as the sum of two abundant numbers."""
+    limit = 28123
+
+    abundant = [i for i in range(12, limit + 1) if check_abundant(i)]
+
+    can_be_written = [False] * (limit + 1)
+    for i in range(len(abundant)):
+        for j in range(i, len(abundant)):
+            s = abundant[i] + abundant[j]
+            if s <= limit:
+                can_be_written[s] = True
+            else:
+                break
+
+    return sum(i for i, x in enumerate(can_be_written) if not x)
+print(answer())
+#o/p: 4179871
+
+def solver(n):
+    """Find the sum of all positive integers
+    that cannot be expressed as the sum of two abundant numbers up to n."""
+    if n < 1:
+        return 0
+
+    abundant = [i for i in range(12, n + 1) if check_abundant(i)]
+
+    can_be_written = [False] * (n + 1)
+    for i in range(len(abundant)):
+        for j in range(i, len(abundant)):
+            s = abundant[i] + abundant[j]
+            if s <= n:
+                can_be_written[s] = True
+            else:
+                break
+
+    return sum(i for i, x in enumerate(can_be_written) if not x)
+
+print(solver(100))
+#o/p: 2766
