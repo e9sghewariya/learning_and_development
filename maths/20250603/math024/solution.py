ab713d1332c0ba559fd5b337c60f2c13330f0ec8
e9sghewariya <sghewariya@enine.dev>
2025-06-04 03:13:10 +0530

    feat(math024): Project Euler problem 24 --(part A): function to find millionth lexicographic permutation of digit 0-9 --(part B): generic function to find nth lexicographic permutation

--- maths/20250603/math024/solution.py
+++ maths/20250603/math024/solution.py
@@ -5,18 +5,6 @@ and a generalized function to find the nth lexicographic permutation of any sequ
 """
 import math
 
-
-def answer():
-    """Find the millionth lexicographic permutation of digits 0-9."""
-    digits = list("0123456789")
-    k = 1000000
-    return get_kth_permutation(digits, k)
-
-def solver(seq="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", n=1):
-    """Find the nth lexicographic permutation of a given sequence."""
-    seq_list = list(seq)
-    return get_kth_permutation(seq_list, n)
-
 def get_kth_permutation(seq, k):
     """Helper function to Find the k-th lexicographic permutation of a sequence."""
     seq = sorted(seq)
@@ -31,3 +19,15 @@ def get_kth_permutation(seq, k):
         k %= fact
 
     return ''.join(result)
+
+def answer():
+    """Find the millionth lexicographic permutation of digits 0-9."""
+    digits = list("0123456789")
+    k = 1000000
+    return get_kth_permutation(digits, k)
+
+def solver(seq="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", n=1):
+    """Find the nth lexicographic permutation of a given sequence."""
+    seq_list = list(seq)
+    return get_kth_permutation(seq_list, n)
+
