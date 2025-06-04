55c538302dc01a607982e43bfa76940bd9f61d8b
e9sghewariya <sghewariya@enine.dev>
2025-06-04 09:35:54 +0530

    fix: update file with correct syntax of tuple

--- python/practice_tuple.py
+++ python/practice_tuple.py
@@ -2,5 +2,5 @@
 # practice_tuple.py
 This script generates a list of even numbers from 0 to 100"""
 
-single_value_tuple = (42)
+single_value_tuple = (42,)
 print("Tuple with one value:", single_value_tuple)
