0e666ff909e5a4d13a6d53e8c982c09b5b4c81e7
e9sghewariya <sghewariya@enine.dev>
2025-06-04 08:22:44 +0530

    feat: generate list of even numbers up to 100

---
+++ python/practice_list.py
@@ -0,0 +1,10 @@
+"""
+This script generates a list of even numbers from 0 to 100  """
+
+even_list = []
+
+for i in range(0, 101, 2):
+    even_list.append(i)
+
+for index, num in enumerate(even_list, start=1):
+    print(f"{index}: {num}")
