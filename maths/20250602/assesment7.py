1a710a95c99403f5114a51f9c771c7c7091e1ecc
e9sghewariya <sghewariya@enine.dev>
2025-06-06 05:56:39 +0530

    fix: resolved pylint errors

--- maths/20250602/assesment7.py
+++ maths/20250602/assesment7.py
@@ -22,7 +22,30 @@
 # ```
 # solver(n)
 # ```
-from ..utils import check_prime
+
+import math
+
+
+def check_prime(num):
+    """
+    Check if a number is prime.
+
+    Args:
+        num (int): The number to check.
+
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(math.isqrt(num)) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
 
 
 def answer():
